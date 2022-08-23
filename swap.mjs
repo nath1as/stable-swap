5bc8b6223f421f1a2fa17b026e32ac19df838b1b
nathias <n@th1.as>
2022-08-23 12:03:19 +0200

    Parse csv

--- swap.mjs
+++ swap.mjs
@@ -1,12 +1,26 @@
-
-import fs from "fs";
+import fs from 'fs'
+// D invariant calculation
+// A * sum(x_i) * n**n + D = A * D * n**n + D**(n+1) / (n**n * prod(x_i))
+//Converging solution:
+// D[j+1] = (A * n**n * sum(x_i) - D[j]**(n+1) / (n**n prod(x_i))) / (A * n**n - 1)
 
 const getFiles = () => {
-    const path = `StableSwap-test-2.csv`;
-    const file = fs.readFileSync(path);
-    const fc = file.toString();
+  const path = `StableSwap-test-2.csv`
+  const file = fs.readFileSync(path)
+  const fc = file.toString()
 
-  console.log(path, file, fc)
-};
+  const rows = fc.split('\n')
+  rows.forEach((row, i) => {
+    const a = row.split(",");
+    const A = a[0];
+    const x = a[1];
+    const y = a[2];
+    const exchngX = a[3];
+    const exchngY = null; 
+    
+    console.log(i, A, x, y, exchngX );
+  }
+  )
+}
 
-getFiles();
+getFiles()
